259f4b8c4da5f925f05ceb6b7a52f76f09a61716
juangd90 <88008431+juangd90@users.noreply.github.com>
2021-09-15 08:43:59 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -20,13 +20,11 @@ def listarPedidos(request):
          pedido_list=Pedido.objects.filter(usuario__dni=dni)
          #filtro de los pedidos todos los que coinciden con el dni ingresado        
 
-          #trae ok el dni del formulario
-          # falta hacer la logica y comparar para mostrar el resultado  
+       
          return render(request,"app/resultados.html",{
                    "pedido_list":pedido_list    
                  })
-        #se puede crear un objeto de tipo pedido para ir agregando ahi todos los resultados
-        # al final se mostraria solamente ese objeto con un for en el html           
+                  
 
      else:        
         return render(request,"app/busqueda.html")
