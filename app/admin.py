e16f6a1647312eff86f703aa1aa8c8eb4a63fdb5
juangd90 <88008431+juangd90@users.noreply.github.com>
2021-10-16 10:15:49 -0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from .models import Pedido,Usuario,pedidoNoUsuario
 # Register your models here
-class CategoryAdmin(admin.ModelAdmin):
-    list_display=("name",)
+class PedidoNoUsuarioAdmin(admin.ModelAdmin):
+    list_display=("nombre_y_apellido","domicilio","celular","comentarios","retiro_en_domicilio")
 
-class QuestionAdmin(admin.ModelAdmin):
-    list_display=("texto_pregunta","categoria")
+
+
 
 admin.site.register(Pedido)
 admin.site.register(Usuario)
-admin.site.register(pedidoNoUsuario)
+admin.site.register(pedidoNoUsuario,PedidoNoUsuarioAdmin)
